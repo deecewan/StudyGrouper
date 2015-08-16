44e386cc515041db3497e8dae3f9f2187a8a1f16
David <david.buchanswanson@gmail.com>
2015-08-16 21:22:53 +1000

    config updated

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ __author__ = 'David'
 #MongoEngine Settings
 
 MONGODB_SETTINGS = {
-    # 'host' : '102.197.65.175',
-    'db' : 'StudyGrouper'
-
+    'host' : 'mongodb://study:a2lvTnFla2ZRQ0U0OWxZRFR6cmwyQ1JNb2NkK3dkNXdFL0NZWENBa1cxOD0K@172.17.0.81:27017/study-production'
 }
 
 SECRET_KEY = 'shh, secret'
